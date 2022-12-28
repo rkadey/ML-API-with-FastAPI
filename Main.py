c71f08f02382232301b9a9d47d00af3a25fa6243
Richard Kadey <richardkadey@gmail.com>
2022-12-28 01:29:51 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+import joblib
+import uvicorn
+from pydantic import BaseModel
 
-app = FastAPI()
+
+app = FastAPI(title ='Credit Card Churn Prediction', version = 1.0, description = 'Classification Machine Learning Prediction')
+model = joblib.load("joblib_CC_Model.pkl")
+
+class Data(BaseModel):
 
 
-@app.get("/")
-async def ml_api():
-    return {"Tesing": " First App"}
+
+ @app.post("/predict")
+
+ async def predict(data):
+    prediction = model.predict()
+    return prediction
+
+if __name__ == '__main__':
+    uvicorn.run("Main:app", reload = True)
